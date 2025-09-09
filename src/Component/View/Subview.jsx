1cecc727806fb3fb769f7294f88a95918cd6e30b
rakib hasan <rakibhasanmd457@gmail.com>
2025-09-10 00:01:33 +0600

    add to the card section

--- src/Component/View/Subview.jsx
+++ src/Component/View/Subview.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
+import Swal from 'sweetalert2';
+import { CgKey } from 'react-icons/cg';
 
 export default function Subview({ data }) {
     const { _id, price, name, quantity, photo } = data
+
+    const heldeldete = _id => {
+        console.log(_id, "this is a deletet id")
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+
+
+            fetch(`http://localhost:5000/returncoffee/${_id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (result.isConfirmed) {
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+
+                    }
+                })
+
+
+        });
+    }
     return (
         <div>
             <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
@@ -15,7 +50,7 @@ export default function Subview({ data }) {
                 </div>
                 <p className="dark:text-gray-800">Price: {data.price}</p>
                 <div className='flex gap-5'>
-                    <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"><RiDeleteBin5Line ></RiDeleteBin5Line ></button>
+                    <button onClick={() => heldeldete(_id)} type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"><RiDeleteBin5Line ></RiDeleteBin5Line ></button>
                     <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"><FaRegEdit ></FaRegEdit ></button>
                 </div>
             </div>
