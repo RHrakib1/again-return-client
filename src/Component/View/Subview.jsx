89aebb28dee9bc715ed342ce6176fa4d24c04bff
rakib hasan <rakibhasanmd457@gmail.com>
2025-09-09 23:36:02 +0600

    add to the card section

---
+++ src/Component/View/Subview.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+
+export default function Subview({ data }) {
+    const { _id, price, name, quantity, photo } = data
+    return (
+        <div>
+            <div className="max-w-xs p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+                <img src={data.photo} alt="" className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
+                <div className="mt-6 mb-2">
+                    <span className="block text-xs font-medium tracking-widest uppercase dark:text-violet-600">
+                        Quantity: {data.quantity}</span>
+                    <h2 className="text-xl font-semibold tracking-wide">Name: {data.name}</h2>
+                </div>
+                <p className="dark:text-gray-800">Price: {data.price}</p>
+                <div className='flex gap-5'>
+                    <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"><RiDeleteBin5Line ></RiDeleteBin5Line ></button>
+                    <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50"><FaRegEdit ></FaRegEdit ></button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Component/View/View.jsx
+++ src/Component/View/View.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import Subview from './Subview';
+
+
 
 export default function View() {
     const loaddata = useLoaderData()
     return (
         <div>
             <h1>view is the best option on the world {loaddata.length} Data only</h1>
+            <div className='grid grid-cols-4 gap-5 max-w-[1100px] mx-auto'>
+                {
+                    loaddata.map(data => <Subview data={data}></Subview>)
+                }
+            </div>
         </div>
     )
 }
